8377a8f0e1143f6d12309c6ecda0c14f110929ff
ranjitp2001 <ranjitoff2001@gmail.com>
2024-07-07 09:58:13 +0530

    Delete Olympic Data Analysis/DataExtraction/DataExtraction directory

--- Olympic Data Analysis/DataExtraction/DataExtraction/BeerPrice(500ml).R
+++
@@ -1,7 +0,0 @@
-beer <- read.csv("Data/BeerPrice.csv")
-beer <- beer[, c(8, 24)]
-
-names(beer) <- c("Nation", "BeerPrice")
-# 500ml beer price in usd
-
-save(beer, file = "beer.Rdata")
--- Olympic Data Analysis/DataExtraction/DataExtraction/forestArea.R
+++
@@ -1,7 +0,0 @@
-load("Data/forest.Rdata")
-
-## Need to clean the dataset
-
-data_forest$forest_area <- as.numeric(data_forest$forest_area)
-
-save(data_forest, file = "forestArea.Rdata")
